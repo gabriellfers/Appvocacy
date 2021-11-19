ca6a79ba63197d62c49c1120fe518dc003498f62
Gabriel Fernandes <gabrielfernandes0609@gmail.com>
2021-11-18 22:54:36 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import Cadastro from './src/telas/CadastroEscolha';
 import CadastroCliente from './src/telas/CadastroCliente';
+import CadastroAdvogado from './src/telas/CadastroAdvogado';
 import Main from './src/telas/rotas';
 import Login from './src/telas/Login';
 import Perfil from './src/telas/Perfil';
@@ -22,37 +23,42 @@ export default function App({  }){
         <Stack.Screen 
         name="Cadastro"
         component={Cadastro}
-        options={{title: "Appvocacy", headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerShown: true}}
         />
         <Stack.Screen 
         name="Main"
         component={Main}
-        options={{title: "Appvocacy", headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerLeft: false, headerShown: true}}
         />
         <Stack.Screen 
         name="Login"
         component={Login}
-        options={{title: "Appvocacy", headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerShown: true}}
         />
         <Stack.Screen 
         name="CadastroCliente"
         component={CadastroCliente}
-        options={{title: "Appvocacy" ,headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerShown: true}}
+        />
+        <Stack.Screen 
+        name="CadastroAdvogado"
+        component={CadastroAdvogado}
+        options={{title: "Appvocacy", headerShown: true}}
         />
         <Stack.Screen 
         name="Perfil"
         component={Perfil}
-        options={{title: "Appvocacy", headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerShown: true}}
         />
         <Stack.Screen 
         name="Buscar"
         component={Buscar}
-        options={{title: "Appvocacy", headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerShown: true}}
         />
         <Stack.Screen 
         name="Chats"
         component={Chats}
-        options={{title: "Appvocacy", headerLeft: null, headerShown: true}}
+        options={{title: "Appvocacy", headerShown: true}}
         />
         
        </Stack.Navigator>
